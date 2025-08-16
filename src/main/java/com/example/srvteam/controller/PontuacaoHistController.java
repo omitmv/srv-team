afe80709a98feb36161d3605241fe42de200448f
Rafael Valverde <omit.mv@gmail.com>
2025-08-16 13:40:21 -0300

    Correções e  melhorias

--- src/main/java/com/example/srvteam/controller/PontuacaoHistController.java
+++ src/main/java/com/example/srvteam/controller/PontuacaoHistController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.srvteam.dto.request.PontuacaoHistPorPosicaoRequest;
 import com.example.srvteam.dto.request.PontuacaoHistRequest;
 import com.example.srvteam.dto.response.PontuacaoHistResponse;
 import com.example.srvteam.service.PontuacaoHistService;
